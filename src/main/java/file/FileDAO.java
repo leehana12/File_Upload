e1a472d7f22cd07b0291ca438bcb37e5f3f196fa
leehana <lubihana12@gmail.com>
2024-03-13 16:17:49 +0900

    FileDAO.java -> 데이터베이스 연동 코드 작성

--- src/main/java/file/FileDAO.java
+++ src/main/java/file/FileDAO.java
@@ -1,5 +1,37 @@
 package file;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class FileDAO {
 
+	private Connection conn;
+	
+	public FileDAO() {
+		
+		try { // 데이터 베이스 연동
+			String dbURL = "jdbc:mysql://localhost:3306/file";
+			String dbID = "root";
+			String dbPassword = "12345";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int uploae(String fileName, String fileRealName) {
+		String SQL = "insert into file valuse(?,?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);// Statement=SQL구문실행 역할 -> PreparedStatement는 Statement클래스의 기능 향상
+			pstmt.setString(1, fileName);
+			pstmt.setString(2, fileRealName);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 정수 리턴값을 사용하는 경우 0:거짓, 1:참 / 1:검색 후, 0:검색 , -1:검색전
+	}
+	
 }
